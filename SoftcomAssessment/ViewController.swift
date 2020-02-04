79c2f823067b3dc8ea021abd0bd4653c6cff840d
Oluwatobi Omotayo <tobitech@ymail.com>
2020-02-04 15:39:45 +0100

    Initial Commit

---
+++ SoftcomAssessment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoftcomAssessment
+//
+//  Created by Oluwatobi Omotayo on 04/02/2020.
+//  Copyright © 2020 Oluwatobi Omotayo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
